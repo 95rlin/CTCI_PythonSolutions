88489ddfe3159df0099bd0e80723e10b75cef7d9
Ray Lin <95rlin@gmail.com>
2021-11-17 00:11:30 -0800

    Modify solutions

--- Chapter1/1_1_isUnique.py
+++ Chapter1/1_1_isUnique.py
@@ -1,22 +1,23 @@
 from collections import Counter
 
 
-def isUnique(arg):
-	# input = abcdeeee
-	# result = false 
-	cnt = Counter(arg)
-	for i in cnt.values():
-		if i > 1:
-			return False 
-	return True 
+def is_unique(arg):
+    # input = abcdeeee
+    # result = false
+    cnt = Counter(arg)
+    for i in cnt.values():
+        if i > 1:
+            return False
+    return True
 
-	# Runtime = n + n == O(n)
-def isUnique2(arg):
-	return len(set(arg)) == len(arg)
 
-	# Runtime = O(n)
+# Runtime = n + n == O(n)
+def is_unique2(arg):
+    return len(set(arg)) == len(arg)
+
+# Runtime = O(n)
+
 
 if __name__ == '__main__':
-	print(isUnique("abcde"))
-	print(isUnique2("abcde"))
-	
+    print(is_unique("abcde"))
+    print(is_unique2("abcde"))
--- Chapter1/1_5_one_away.py
+++ Chapter1/1_5_one_away.py
@@ -114,6 +114,7 @@ class Test(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    print("finished!")
 
     #print(one_away("ale", "elas"))
 
