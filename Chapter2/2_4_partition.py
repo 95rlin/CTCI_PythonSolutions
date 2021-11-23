dbc76d079533cd4660f93249c268a595302bb56f
Ray Lin <95rlin@gmail.com>
2021-11-22 23:28:05 -0800

    Added solution for problem 2.4 (partition)

--- Chapter2/2_4_partition.py
+++ Chapter2/2_4_partition.py
@@ -1,12 +1,29 @@
 from linked_list import LinkedList
-	
-def partition(ll, val):
-	print(val)
+from linked_list import LinkedListNode
+def partition(head, val):
+        ltail = lhead = LinkedListNode(0)
+        rtail = rhead = LinkedListNode(0)
+        
+        while(head):
+            if head.value < val:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+            
+        ltail.next = rhead.next
+        rtail.next = None
+        return lhead.next
+        
 
 
 if __name__ == "__main__":
     ll = LinkedList.generate(10, 0, 9)
     print(ll)
-    partition(ll, ll.head.value)
-   
-    
+    new = partition(ll.head, ll.head.value)
+
+    while(new):
+    	print(str(new.value) + " -> ", end='')
+    	new = new.next
---
+++ Extra/test2.py
@@ -0,0 +1,4 @@
+stack = [("test",False)]
+test1, test2 = stack.pop()
+print(test1)
+print(test2)
